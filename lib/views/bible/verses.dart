6af8dd6889435888ed9d93ebbabdd9073ce0144b
Temitope Daniel <31567966+ologunB@users.noreply.github.com>
2023-08-13 20:08:59 +0100

    list done

--- lib/views/bible/verses.dart
+++ lib/views/bible/verses.dart
@@ -20,51 +20,59 @@ class _VersesScreenState extends State<VersesScreen> {
   late String book;
   late int chapter, prevChapter, nextChapter;
   List<Verse> verses = [];
-  final double endReachedThreshold = 300.0;
   final ItemScrollController itemScrollController = ItemScrollController();
-  final ScrollOffsetController scrollOffsetController =
-      ScrollOffsetController();
+
   final ItemPositionsListener itemPositionsListener =
       ItemPositionsListener.create();
-  final ScrollOffsetListener scrollOffsetListener =
-      ScrollOffsetListener.create();
-  ScrollController controller = ScrollController();
 
+  ScrollController controller = ScrollController();
   @override
   void initState() {
     super.initState();
     book = widget.book;
     chapter = widget.chapter;
     getVerses();
-    itemPositionsListener.itemPositions.addListener(_positionListener);
+
+    Future.delayed(const Duration(milliseconds: 1500), () {
+      itemPositionsListener.itemPositions.addListener(_positionListener);
+    });
   }
+
+  String? oldChapter, currentChapter;
 
   _positionListener() {
     List<ItemPosition> li = itemPositionsListener.itemPositions.value.toList();
+    li.sort((a, b) => a.index.compareTo(b.index));
     if (li.firstWhereOrNull((e) => e.index == 10) != null) {
-      print("Close to top");
       List<Verse> data = objectbox.get2ChaptersBefore(prevChapter);
       if (data.isNotEmpty) {
+        print("Close to top, first chapter is ${data.first.chapterName}");
         prevChapter = prevChapter - 2;
         verses.addAll(data);
         verses = verses.toSet().toList();
-        setState(() {});
         verses.sort((a, b) => a.absoluteVerse.compareTo(b.absoluteVerse));
-        itemScrollController.jumpTo(index: data.length + 11, alignment: .5);
+        itemScrollController.jumpTo(index: data.length + li.first.index);
+        setState(() {});
       }
     }
 
     if (li.firstWhereOrNull((e) => e.index == verses.length - 10) != null) {
-      print("Close to bottom");
       List<Verse> data = objectbox.get2ChaptersAfter(nextChapter);
       if (data.isNotEmpty) {
+        print("Close to bottom, last chapter is ${data.last.chapterName}");
         nextChapter = nextChapter + 2;
         verses.addAll(data);
         verses = verses.toSet().toList();
         verses.sort((a, b) => a.absoluteVerse.compareTo(b.absoluteVerse));
-
         setState(() {});
       }
+    }
+
+    li.sort((a, b) => a.index.compareTo(b.index));
+    oldChapter = verses[li.first.index].chapterName;
+    if (oldChapter != currentChapter) {
+      currentChapter = oldChapter;
+      setState(() {});
     }
   }
 
@@ -96,171 +104,167 @@ class _VersesScreenState extends State<VersesScreen> {
     });
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                SizedBox(width: 25.h),
-                IntrinsicHeight(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xffEAEAEA),
-                      borderRadius: BorderRadius.circular(40.h),
-                    ),
-                    child: Row(
-                      children: [
-                        InkWell(
-                          onTap: () async {
-                            dynamic a = await showModalBottomSheet(
-                              backgroundColor: Colors.white,
-                              context: context,
-                              isScrollControlled: true,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.only(
-                                  topRight: Radius.circular(50.h),
-                                  topLeft: Radius.circular(50.h),
-                                ),
-                              ),
-                              builder: (c) {
-                                return SelectChapterDialog(name: book);
-                              },
-                            );
-                            if (a != null) {
-                              book = a.first;
-                              chapter = a.last;
-                              setState(() {});
-                              getVerses();
-                            }
-                          },
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30.h),
-                            bottomLeft: Radius.circular(30.h),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.all(10.h),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                HexText(
-                                  book,
-                                  fontSize: 16.sp,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.normal,
-                                ),
-                                SizedBox(width: 10.h),
-                                Image.asset(
-                                  'down'.png,
-                                  height: 32.h,
-                                  color: AppColors.black,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        const VerticalDivider(width: 0, thickness: 0),
-                        InkWell(
-                          onTap: () {},
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30.h),
-                            bottomLeft: Radius.circular(30.h),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.all(10.h),
-                            child: HexText(
-                              'Books',
-                              fontSize: 16.sp,
-                              color: Colors.black,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+      appBar: PreferredSize(
+        preferredSize: Size(0, 50.h),
+        child: SafeArea(
+          child: Row(
+            children: [
+              SizedBox(width: 25.h),
+              IntrinsicHeight(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xffEAEAEA),
+                    borderRadius: BorderRadius.circular(40.h),
                   ),
-                ),
-                const Spacer(),
-                InkWell(
-                  onTap: () {},
-                  child: Image.asset(
-                    'aa'.png,
-                    height: 32.h,
-                  ),
-                ),
-                SizedBox(width: 24.h),
-                InkWell(
-                  onTap: () {
-                    push(context, const SearchPage(), true);
-                  },
-                  child: Image.asset(
-                    'search'.png,
-                    height: 32.h,
-                    color: AppColors.black,
-                  ),
-                ),
-                SizedBox(width: 25.h),
-              ],
-            ),
-            HexText(
-              '${a.join(' || ')}... total: ${a.length}',
-              fontSize: 16.sp,
-              color: Colors.black,
-              align: TextAlign.center,
-              fontWeight: FontWeight.bold,
-            ),
-            SizedBox(height: 15.h),
-            Expanded(
-              child: ScrollablePositionedList.separated(
-                itemCount: verses.length,
-                shrinkWrap: true,
-                separatorBuilder: (_, __) => SizedBox(height: 10.h),
-                padding: EdgeInsets.symmetric(horizontal: 25.h),
-                itemBuilder: (context, index) {
-                  Verse element = verses[index];
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  child: Row(
                     children: [
-                      if (element.verse == 1)
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              height: 40.h,
-                              alignment: Alignment.center,
-                              padding: EdgeInsets.symmetric(horizontal: 8.h),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(8.h),
+                      InkWell(
+                        onTap: () async {
+                          dynamic a = await showModalBottomSheet(
+                            backgroundColor: Colors.white,
+                            context: context,
+                            isScrollControlled: true,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(50.h),
+                                topLeft: Radius.circular(50.h),
                               ),
-                              child: HexText(
-                                element.chapterName,
+                            ),
+                            builder: (c) {
+                              return SelectChapterDialog(name: book);
+                            },
+                          );
+                          if (a != null) {
+                            book = a.first;
+                            chapter = a.last;
+                            setState(() {});
+                            getVerses();
+                          }
+                        },
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30.h),
+                          bottomLeft: Radius.circular(30.h),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(10.h),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              HexText(
+                                book,
                                 fontSize: 16.sp,
                                 color: Colors.black,
-                                align: TextAlign.center,
-                                fontWeight: FontWeight.bold,
+                                fontWeight: FontWeight.normal,
                               ),
-                            ),
-                          ],
+                              SizedBox(width: 10.h),
+                              Image.asset(
+                                'down'.png,
+                                height: 32.h,
+                                color: AppColors.black,
+                              ),
+                            ],
+                          ),
                         ),
-                      HexText(
-                        '${element.verse}. ${element.text}',
-                        fontSize: 16.sp,
-                        color: Colors.black,
-                        key: ValueKey(element.absoluteVerse),
-                        fontWeight: FontWeight.w400,
-                      )
+                      ),
+                      const VerticalDivider(width: 0, thickness: 0),
+                      InkWell(
+                        onTap: () {},
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30.h),
+                          bottomLeft: Radius.circular(30.h),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(10.h),
+                          child: HexText(
+                            'Books',
+                            fontSize: 16.sp,
+                            color: Colors.black,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
                     ],
-                  );
-                },
-                itemScrollController: itemScrollController,
-                scrollOffsetController: scrollOffsetController,
-                itemPositionsListener: itemPositionsListener,
-                scrollOffsetListener: scrollOffsetListener,
+                  ),
+                ),
               ),
-            )
-          ],
+              const Spacer(),
+              InkWell(
+                onTap: () {},
+                child: Image.asset(
+                  'aa'.png,
+                  height: 32.h,
+                ),
+              ),
+              SizedBox(width: 24.h),
+              InkWell(
+                onTap: () {
+                  push(context, const SearchPage(), true);
+                },
+                child: Image.asset(
+                  'search'.png,
+                  height: 32.h,
+                  color: AppColors.black,
+                ),
+              ),
+              SizedBox(width: 25.h),
+            ],
+          ),
         ),
       ),
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          ScrollablePositionedList.separated(
+            itemCount: verses.length,
+            shrinkWrap: true,
+            separatorBuilder: (_, __) => SizedBox(height: 10.h),
+            padding: EdgeInsets.symmetric(horizontal: 25.h),
+            itemBuilder: (context, index) {
+              Verse element = verses[index];
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (element.verse == 1) header(element.chapterName),
+                  HexText(
+                    ' ${element.verse}. ${element.text}',
+                    fontSize: 16.sp,
+                    color: Colors.black,
+                    key: ValueKey(element.absoluteVerse),
+                    fontWeight: FontWeight.w400,
+                  )
+                ],
+              );
+            },
+            itemScrollController: itemScrollController,
+            itemPositionsListener: itemPositionsListener,
+          ),
+          if (currentChapter != null) header(currentChapter!)
+        ],
+      ),
+    );
+  }
+
+  Widget header(String a) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 40.h,
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(horizontal: 8.h),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8.h),
+          ),
+          child: HexText(
+            a,
+            fontSize: 16.sp,
+            color: Colors.black,
+            align: TextAlign.center,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
   }
 }
