9a41eed24ff6efbaa27d043fd2eaee9abad0ddbd
Temitope Daniel <31567966+ologunB@users.noreply.github.com>
2023-09-13 20:54:05 +0100

    add dynamic links

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
-import 'package:hexcelon/views/auth/onboard_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 
 import 'core/models/navigator.dart';
+import 'core/models/post_model.dart';
 import 'core/storage/local_storage.dart';
 import 'locator.dart';
+import 'views/auth/onboard_view.dart';
 import 'views/create/media.dart';
+import 'views/home/post_details.dart';
 import 'views/home/user_layout.dart';
 import 'views/widgets/hex_text.dart';
 
@@ -11,6 +15,7 @@ List<GlobalKey<NavigatorState>> navigatorKeys = [];
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   setupLocator();
@@ -21,8 +26,32 @@ Future<void> main() async {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    initDynamicLinks();
+  }
+
+  Future<void> initDynamicLinks() async {
+    FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
+    dynamicLinks.onLink.listen((dynamicLinkData) {
+      Map<String, String> d = dynamicLinkData.link.queryParameters;
+      Post p = Post.fromJson(jsonDecode(d['data']!));
+      if (p.title == null) return;
+      push(AppNavigator.navKey.currentContext!, PostDetailScreen(post: p));
+    }).onError((error) {
+      print('onLink error');
+      print(error.message);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
